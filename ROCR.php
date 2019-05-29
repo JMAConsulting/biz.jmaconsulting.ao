4bd16a8e1ed9381d00ba0cc404138e24e71e2eb2
Monish Deb <deb.monish@gmail.com>
2019-05-29 22:21:15 +0530

    add import script to redo batches

--- ROCR.php
+++ ROCR.php
@@ -2,7 +2,7 @@
 
 Class CRM_ROCR_Import {
 
-  public $civicrmPath = '/var/www/jma.staging.autismontario.com/htdocs/vendor/civicrm/civicrm-core/';
+  public $civicrmPath = '/var/www/autismontario.com/htdocs/vendor/civicrm/civicrm-core/';
   //public $civicrmPath = '/home/edsel/public_html/test/sites/all/modules/civicrm/';
   public $sourceContactId = '';
 
@@ -16,6 +16,42 @@ Class CRM_ROCR_Import {
     require_once $civicrmPath .'civicrm.config.php';
     require_once $civicrmPath .'CRM/Core/Config.php';
     $config = CRM_Core_Config::singleton();
+  }
+
+  function redoBatches() {
+    $contactID = CRM_Core_Session::getLoggedInContactID();
+    $sql = "
+      SELECT ee.batch_id, b.title, ee.payment_processor_id
+       FROM `civicrm_easybatch_entity` as ee
+      LEFT JOIN civicrm_batch b ON ee.batch_id = b.id
+      LEFT JOIN civicrm_entity_batch eb ON eb.batch_id = b.id
+      LEFT JOIN civicrm_financial_trxn ft ON ft.id = eb.entity_id
+      WHERE ee.batch_date BETWEEN '2019-04-1' AND '2019-05-30' AND ee.payment_processor_id IS NOT NULL  AND ee.payment_processor_id = 3 GROUP BY ee.batch_id ";
+      $dao = CRM_Core_DAO::executeQuery($sql);
+
+      while($dao->fetch()) {
+        civicrm_api3('Batch', 'create', [
+          'id' => $dao->batch_id,
+          'title' => str_replace('Auto', 'Visa Auto', $dao->title),
+        ]);
+        CRM_Core_DAO::executeQuery("UPDATE civicrm_easybatch_entity SET card_type_id = 1 WHERE batch_id = " . $dao->batch_id);
+        $batchID = civicrm_api3('Batch', 'create', [
+          'title' => str_replace('Auto', 'MasterCard Auto', $dao->title),
+          'status_id' => "Closed",
+          'type_id' => "Contribution",
+        ])['id'];
+
+        CRM_Core_DAO::executeQuery("INSERT INTO civicrm_easybatch_entity (`batch_id`, `contact_id`, `payment_processor_id`, `is_automatic`, `batch_date`, `card_type_id`)
+         VALUES($batchID, $contactID,  $dao->payment_processor_id, 1, '" . date('Y-m-d H:i:s'). "', 2)");
+
+         $sql = "INSERT IGNORE INTO civicrm_entity_batch (`entity_table`, `entity_id`, `batch_id`)
+           SELECT 'civicrm_financial_trxn', eb.entity_id, $batchID
+            FROM civicrm_entity_batch eb
+            LEFT JOIN civicrm_financial_trxn ft ON ft.id = eb.entity_id
+            AND eb.batch_id = $dao->batch_id AND ft.card_type_id = 2
+         ";
+         CRM_Core_DAO::executeQuery($sql);
+      }
   }
 
   function rocrImport() {
@@ -379,4 +415,5 @@ Class CRM_ROCR_Import {
 }
 
 $import = new CRM_ROCR_Import();
-$import->rocrImport();
+//$import->rocrImport();
+$import->redoBatches();
