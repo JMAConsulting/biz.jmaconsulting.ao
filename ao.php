64dc7fb80a4fe9b0fc642ab109e405fa1771af2c
Monish Deb <deb.monish@gmail.com>
2019-06-16 11:49:54 +0530

    minor fix

--- ao.php
+++ ao.php
@@ -463,7 +463,7 @@ function ao_civicrm_preProcess($formName, &$form) {
   $msg = NULL;
   if ('CRM_Contribute_Form_AdditionalPayment' == $formName) {
     if ($form->getVar('_paymentType') == 'refund') {
-      if ($form->_component == 'event') {
+      if ($form->getVar('component') == 'event') {
         if ($form->_mode) {
           if (!CRM_Core_Permission::check('process event refund')) {
             $msg = ts('You do not have permission to process refund for an event');
